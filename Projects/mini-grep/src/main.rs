2e8a27d6d5524bc5f92ffad15016f115b09b7fc7
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-08-30 17:25:11 +0800

    feat: finish initial version for minigrep

--- Projects/mini-grep/src/main.rs
+++ Projects/mini-grep/src/main.rs
@@ -1,13 +1,16 @@
-use std::{env, fs};
+use mini_grep::{Config, run};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("Searching for \"{}\" in file {}...", config.query, config.file_path);
 
-    let contents =
-        fs::read_to_string(file_path).expect("Error: should have been able to read the file");
-    println!("With contents: {}", contents)
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
